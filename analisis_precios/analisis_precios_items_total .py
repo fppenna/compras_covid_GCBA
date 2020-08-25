cf905287fb77ea4200d0f08126ed7692357b715f
fppenna <41331853+fppenna@users.noreply.github.com>
2020-08-25 16:23:12 -0300

    Update analisis_precios_items_total .py

--- analisis_precios/analisis_precios_items_total .py
+++ analisis_precios/analisis_precios_items_total .py
@@ -2,16 +2,16 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-# Defining series scaler
+# Definir objeto con la función StandardScaler para utilizar posteriormente
 scaler = StandardScaler()
 def escalador_serie(serie):
     return scaler.fit_transform(serie.values.reshape(-1, 1)).reshape(1, -1).tolist()[0]
 
-# Loading csv
+# Cargar csv
 input_path='items_analisis_precios.csv'
 items_analisis_precios=pd.read_csv(input_path)
 
-# Scaled price series
+# Escalar series de precios 
 serie_precios_estandarizada=[]
 for i in items_analisis_precios['item'].unique():
     for j in escalador_serie(items_analisis_precios['precio_unitario_actualizado_mayo20'][items_analisis_precios['item']==i]):
@@ -19,11 +19,11 @@ for i in items_analisis_precios['item'].unique():
 
 items_analisis_precios['serie_precios_estandarizada']=pd.Series(serie_precios_estandarizada)
 
-# Procurement method
+# Definir Procurement method
 items_analisis_precios['competencia']=pd.Series()
 items_analisis_precios['competencia'][(items_analisis_precios['tipo_procedimiento']=='CME')|(items_analisis_precios['tipo_procedimiento']=='CDI')|(items_analisis_precios['tipo_procedimiento']=='CDE')]='No competitivo'
 items_analisis_precios['competencia'][(items_analisis_precios['tipo_procedimiento']=='LPU')]='Competitivo'
 
-# Saving csv
+# Serializar datos
 output_path=''
 items_analisis_precios.to_csv('items_analisis_precios_escala.csv', index=False, sep=',', encodign='utf-8')
